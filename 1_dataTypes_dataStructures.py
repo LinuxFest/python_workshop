f8783b9f739dd775e5a1fbf8d1e3ec79027b2cd5
SajadAzami <sajjadaazami@gmail.com>
2016-05-11 02:45:23 +0430

    printaro doros bezar dige hammal

--- 1_dataTypes_dataStructures.py
+++ 1_dataTypes_dataStructures.py
@@ -18,7 +18,7 @@ May 2016
 ####################################################
 
 # print
-print "hello world"
+print ("hello world")
 
 # basic data types
 integer_var = 2
@@ -29,29 +29,29 @@ age = 20
 c1 = 1 + 2j  # complex numbers
 c2 = 2.5 + 4j
 
-print "This is an integer variable: %d" % integer_var
-print "This is an float variable: %f" % float_var
-print "This is an string variable: %s" % string_var
-print "%s is %d years old." % (name, age)
-print c1 + c2
-print c1 * c2
+print ("This is an integer variable: %d" % integer_var)
+print ("This is an float variable: %f" % float_var)
+print ("This is an string variable: %s" % string_var)
+print ("%s is %d years old." % (name, age))
+print (c1 + c2)
+print (c1 * c2)
 
-print string_var[2:9]  # print from second index to ninth index (index begins from 0)
-print name[:-1]  # remove last character
+print (string_var[2:9])  # print from second index to ninth index (index begins from 0)
+print (name[:-1])  # remove last character
 
-print name[::-1]  # reverse string
+print (name[::-1])  # reverse string
 
 # lists
 integer_list = [1, 3, 5, 7]
 integer_list.append(9)
 integer_list.insert(2, 4)
-print integer_list
+print (integer_list)
 
 # Stack
 stack_list = [1, 2, 3, 4]
 stack_list.append(5)  # insert item to stack
 num = stack_list.pop()
-print num
+print (num)
 
 # Queue
 from collections import deque
@@ -59,31 +59,31 @@ from collections import deque
 queue_list = deque([1, 2, 3, 4])
 queue_list.append(5)  # insert item to stack
 queue_list.popleft()
-print queue_list
+print (queue_list)
 
 # set
 a = {"Ali", "Akbar", "Asghar", "Ali"}
 b = {"Hassan", "Ali", "Hossein"}
-print a.intersection(b)
+print (a.intersection(b))
 #  print a & b
-print a.union(b)
-print a - b
-print a ^ b
+print (a.union(b))
+print (a - b)
+print (a ^ b)
 
 # dictionaries
 students = {"sina": 9131062, "farzan": 9131069}
 students["sajjad"] = 9231031
 
 for name, std_num in students.iteritems():
-    print "1.name: %s, std_num: %d" % (name, std_num)
+    print ("1.name: %s, std_num: %d" % (name, std_num))
 
 for name, std_num in students.iteritems():
     print("2.name: {}, std_num: {}".format(name, std_num))
 
 del students["sina"]
-print students
+print (students)
 
 # matrix
 matrix_a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print matrix_a
-print zip(*matrix_a)
+print (matrix_a)
+print (zip(*matrix_a))
