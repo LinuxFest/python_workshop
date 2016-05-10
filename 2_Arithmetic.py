9cd805848c71763c5395e8775521b55b235696a8
SajadAzami <sajjadaazami@gmail.com>
2016-05-11 01:13:22 +0430

    Improvements on dataTypes and Aritmetic were done

--- 2_Arithmetic.py
+++ 2_Arithmetic.py
@@ -9,16 +9,18 @@ minus = 4.5 - 6
 mul = 4.6 * 1.6
 div = 12 / 3
 rem = 17 % 5
+quotient = 17 // 5
 square = 6 ** 2
 cube = 4 ** 3
 
-print plus
-print minus
-print mul
-print div
-print rem
-print square
-print cube
+print "plus: {}".format(plus)
+print "minus: {}".format(minus)
+print "mul: {}".format(mul)
+print "div: {}".format(div)
+print "rem: {}".format(rem)
+print "quotient: {}".format(quotient)  # Rounded away from zero (to negative infinity)
+print "square: {}".format(square)
+print "cube: {}".format(cube)
 
 string1 = "s1"
 string2 = "s2"
@@ -35,4 +37,3 @@ print list1 * 2
 print list1[1]
 list1.append(8)
 print list1
-
